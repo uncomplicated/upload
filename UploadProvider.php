7d1d9563764c4a8bea294ae8d492ec267983e9d8
danny <317201583@qq.com>
2022-11-08 14:47:37 +0800

    upload

---
+++ UploadProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Ggss\Upload;
+
+use Illuminate\Support\ServiceProvider;
+
+class UploadProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPublishing();
+        $this->registerMigrations();
+    }
+
+    /**
+     * Register the package's migrations.
+     *
+     * @return void
+     */
+    private function registerMigrations()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        }
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    private function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../database/migrations' => database_path('migrations'),
+            ], 'video-migrations');
+
+        }
+    }
+
+}
---
+++ database/migrations/2022_04_20_185128_create_media_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('video_id',16)->nullable()->comment('阿里云点播id');
+            $table->string('name',64)->comment('名称');
+            $table->string('mime_type',32)->comment('类型');
+            $table->string('path',255)->nullable()->comment('图片地址');
+            $table->integer('size')->default(0)->comment('大小');
+            $table->integer('duration')->default(0)->comment('时间长度');
+            $table->text('extend')->nullable()->comment('扩展字段');
+            $table->timestamps();
+        });
+
+        Schema::create('media_relation', function (Blueprint $table) {
+            $table->id();
+            $table->integer('media_id')->nullable()->comment('媒体ID');
+            $table->morphs('target');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+        Schema::dropIfExists('media_relation');
+    }
+}
